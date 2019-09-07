da4b59c8b0f30a34353cca0f83911ee9b283fe04
Vivever <myselfvivek9@gmail.com>
2019-09-07 22:51:03 +0530

    Fifth Day of Challenge

---
+++ app/src/main/java/com/example/a30_days_of_android/EditText_View.java
@@ -0,0 +1,14 @@
+package com.example.a30_days_of_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditText_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text__view);
+    }
+}
