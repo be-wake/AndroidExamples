023f43a0ee8f1bd3178bab77feb21abb4ecec1da
Tejasvi Soi <31156450+tejasvisoi@users.noreply.github.com>
2019-10-11 09:00:34 +0530

    Add functionalities to the EditText_View and add Buttons (#5)
    * Add functionalities
    
    * Add A Button View with its attributes
    
    * Change in button view documentation

--- app/src/main/java/com/example/a30_days_of_android/EditText_View.java
+++ app/src/main/java/com/example/a30_days_of_android/EditText_View.java
@@ -2,8 +2,11 @@ package com.example.a30_days_of_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
 import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.text.SpannableString;
@@ -44,17 +47,42 @@ public class EditText_View extends AppCompatActivity {
         editText1.setText(arr,0,arr.length);
 
         //Change the color of input Text
+        edittext1.setTextColor(Color.rgb(223,0,0));
         editText1.setTextColor(Color.CYAN);
+        editText1.setTextColor(Color.parseColor("#000000"));
+        editText1.setTextColor(getResources().getColor(R.color.colorPrimary));
 
         //change BackGround color
 
         editText1.setBackgroundColor(Color.YELLOW);
 
         //You can change color of hint, cursor,link;
+        //setHintTextColor
+        edittext1.setHintTextColor(Color.CYAN);
+        editText1.sette
 
         //You can change the color of underline of edit text
         editText1.setBackgroundTintMode(PorterDuff.Mode.OVERLAY);
+        //Change Font style of edittext
+        editText1.setTypeface(null,Typeface.ITALIC);
+        //setMaxLines
+        edittext1.setMaxLines(4);
+        //Change Gravity
+        editText1.setGravity(Gravity.CENTER|Gravity.BOTTOM);
 
+        //Change the size of textView
+        editText1.setTextSize(56);
+        //In pixels
+        editText1.setTextSize(TypedValue.COMPLEX_UNIT_PX,12);
+        //In dp or dip
+        editText1.setTextSize(TypedValue.COMPLEX_UNIT_DIP,11);
+        //In inch
+        edittext1.setTextSize(TypedValue.COMPLEX_UNIT_IN,0.50f);
+        //In points
+        editText1.setTextSize(TypedValue.COMPLEX_UNIT_PT,30);
+        //In mm
+        editText1.setTextSize(TypedValue.COMPLEX_UNIT_MM,30);
+    
         /*
         What is PorterDuff.Mode
 
